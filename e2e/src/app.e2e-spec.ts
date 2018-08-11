cebd3bf51ee9bbef0168806313aa786fda844717
buddhi <buddhi.amigo@live.com>
2018-08-12 00:18:42 +0530

    Fixed e2e test

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -1,14 +1,15 @@
 import { AppPage } from './app.po';
 
 describe('workspace-project App', () => {
-  let page: AppPage;
+	// tslint:disable:indent
+	let page: AppPage;
 
-  beforeEach(() => {
-    page = new AppPage();
-  });
+	beforeEach(() => {
+		page = new AppPage();
+	});
 
-  it('should display welcome message', () => {
-    page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to cicd!');
-  });
+	it('should display welcome message', () => {
+		page.navigateTo();
+		expect(page.getParagraphText()).toEqual('Build test');
+	});
 });
